0ea814eb8c582e775f81d5a1051b29e73e58b949
Isabella den Otter <i.denotter21@gmail.com>
2026-04-22 08:37:37 -0600

    I'm so close

--- main.js
+++ main.js
@@ -66,8 +66,9 @@ function move(element, direction, distance) {
     elementstyle = window.getComputedStyle(element)
     console.log(elementstyle)
     console.log(gamebounds)
+    console.log(elementstyle.movementdirection)
     console.log(elementstyle.getPropertyValue(movementdirection))
-    elementstyle.movementdirection.setPropertyValue((parseInt(elementstyle.getPropertyValue(movementdirection).replace("px","")) + parseInt(movementdistance)) + "px")
+    elementstyle.movementdirection = (parseInt(elementstyle.getPropertyValue(movementdirection).replace("px","")) + parseInt(movementdistance)) + "px"
     let elementheight = elementstyle.height.replace("px","")
     let elementwidth = elementstyle.width.replace("px","")
     console.log(elementheight)
